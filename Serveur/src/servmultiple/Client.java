24731978c66b971df7bf67d45e712902a2fd4560
ASUS <ASUS@ordi>
2018-02-27 17:24:56 +0100

    Ajout d'un tableau de serveurs.

--- Serveur/src/servmultiple/Client.java
+++ Serveur/src/servmultiple/Client.java
@@ -10,14 +10,23 @@ public class Client
 	
 	public static void main(String[] args) 
 	{
-		try 
-		{	
-			Registry registry = LocateRegistry.getRegistry("localhost", 1099);
-			Object h = (Object) registry.lookup ("UnStore");	
-			Scanner sc = new Scanner(System.in);	
-		}catch (Exception e) 
-		{
-			System.out.print(e.getMessage());
+		
+		int nombreServeurs=0; 
+		 // On créé un tableau de serveurs de taille statique 
+		Registry[] registry=new Registry[nombreServeurs];;                 
+		//On parcourt l'ensemble des serveurs disponibles
+		for (int i=0;  i<nombreServeurs; i++){
+			try 
+			{	
+				registry[i] = LocateRegistry.getRegistry("localhost", 1099);
+				/* TODO : Remplacer Object par l'interface*/
+				Object h = (Object) registry[i].lookup ("UnStore");	
+				Scanner sc = new Scanner(System.in);	
+			}catch (Exception e) 
+			{
+				System.out.print(e.getMessage());
+			}
 		}
+		
 	}
 }
