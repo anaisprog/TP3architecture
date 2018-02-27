5ccbe57a9321e1f905307e496520a1be66da485d
ASUS <ASUS@ordi>
2018-02-27 20:57:26 +0100

    derniere version

--- Serveur/src/servmultiple/Client.java
+++ Serveur/src/servmultiple/Client.java
@@ -13,14 +13,14 @@ public class Client
 		
 		int nombreServeurs=200; 
 		 // On créé un tableau de serveurs de taille statique 
-		Registry[] registry=new Registry[nombreServeurs];;                 
+		Registry[] registry=new Registry[nombreServeurs];
+		
 		//On parcourt l'ensemble des serveurs disponibles
 		for (int i=0;  i<nombreServeurs; i++){
 			try 
 			{	
 				registry[i] = LocateRegistry.getRegistry("localhost", 1099);
-				/* TODO : Remplacer Object par l'interface*/
-				InterfaceMoteurTraitementRMI h = (InterfaceMoteurTraitementRMI) registry[i].lookup ("UnStore");	
+				InterfaceMoteurTraitementRMI h = (InterfaceMoteurTraitementRMI) registry[i].lookup ("InterfaceServeurClient");	
 				Scanner sc = new Scanner(System.in);	
 			}catch (Exception e) 
 			{
